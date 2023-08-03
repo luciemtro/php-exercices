2d1d190777d6f374dd9488bf9af916781ffd3025
Lucie Monteiro <lucie.mtro@gmail.com>
2023-08-03 09:54:30 +0200

    ajout repo

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,27 @@
 <?php
 declare(strict_types=1);
+?>
 
-echo 'It works !';
+<h1>Exercices PHP</h1>
+
+<ol>
+    <li>
+        <a href="/variable_types.php">Les Variables</a>
+    </li>
+    <li>
+        <a href="/condition.php">Les Conditions</a>
+    </li>
+    <li>
+        <a href="/tableau.php">Les Tableaux</a>
+    </li>
+    <li>
+        <a href="/loops.php">Les Loops</a>
+    </li>
+    <li>
+        <a href="/fonction.php">Les Fonctions</a>
+    </li>
+    <li>
+        <a href="/formulaire.php">Les Formulaires</a>
+    </li>
+</ol>
+
